18cba2c9bbe4d40982ea2bd1fd85e2ad24eb27e2
Swapnil Magar <140142924+swapnilmagar123@users.noreply.github.com>
2024-10-19 16:22:23 +0530

    Fix gunicorn start command and module name

--- ImageUploader/wsgi.py
+++ ImageUploader/wsgi.py
@@ -9,7 +9,8 @@ https://docs.djangoproject.com/en/5.1/howto/deployment/wsgi/
 import os
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'PhotoLynk.settings')  # Ensure this matches your project structure
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'PhotoLynk.settings')  # This should match your project structure
 
 application = get_wsgi_application()
+
 

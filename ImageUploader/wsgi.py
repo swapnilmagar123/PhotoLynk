677db09396f3232706baac433f593b56f670b45a
Swapnil Magar <140142924+swapnilmagar123@users.noreply.github.com>
2024-10-19 16:17:00 +0530

    Fix WSGI configuration for deployment

--- ImageUploader/wsgi.py
+++ ImageUploader/wsgi.py
@@ -6,11 +6,10 @@ It exposes the WSGI callable as a module-level variable named ``application``.
 For more information on this file, see
 https://docs.djangoproject.com/en/5.1/howto/deployment/wsgi/
 """
-
 import os
-
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ImageUploader.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'PhotoLynk.settings')  # Ensure this matches your project structure
 
 application = get_wsgi_application()
+
